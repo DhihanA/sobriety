1c9cb0661483dae67e91658d9d0db4c32fdb2345
Dhihan Ahmed <dahmed0421@gmail.com>
2024-11-02 20:35:53 -0400

    trying some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,47 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [timeElapsed, setTimeElasped] = useState(0);
 
   return (
     <>
       <div>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{timeElapsed} {timeElapsed === 1 ? 'Day' : 'Days'} Sober</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setTimeElasped((timeElapsed) => timeElapsed + 1)}>
+          Add 1 Day
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <button onClick={() => setTimeElasped(0)}>
+          Reset
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">What is your name?</span>
+        </div>
+        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+        <div className="label">
+        </div>
+      </label>
+
+
+        {/* <div className="relative max-w-sm">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+          </div>
+          <input datepicker id="default-datepicker" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"   />
+        </div> */}
+
     </>
   )
 }
